979d489ef8535745f43f3e40860ad45d86ebc8d4
Asad <Asad>
2024-09-24 01:12:10 +0500

    MArker added

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -4,6 +4,42 @@ import { useMap } from 'react-leaflet/hooks'
 import '../App.css'
 import 'leaflet/dist/leaflet.css';
 import MapUrl from '../assets/map/overlay3.png'
+import Marker1 from '../assets/map/marker.png'
+import L from 'leaflet';
+
+const svgString = `<svg width="25px" height="25px" viewBox="-4 0 36 36" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+    <title>map-marker</title>
+    <desc>Created with Sketch.</desc>
+    <defs>
+
+</defs>
+    <g id="Vivid.JS" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g id="Vivid-Icons" transform="translate(-125.000000, -643.000000)">
+            <g id="Icons" transform="translate(37.000000, 169.000000)">
+                <g id="map-marker" transform="translate(78.000000, 468.000000)">
+                    <g transform="translate(10.000000, 6.000000)">
+                        <path d="M14,0 C21.732,0 28,5.641 28,12.6 C28,23.963 14,36 14,36 C14,36 0,24.064 0,12.6 C0,5.641 6.268,0 14,0 Z" id="Shape" fill="#FF6E6E">
+
+</path>
+                        <circle id="Oval" fill="#0C0058" fill-rule="nonzero" cx="14" cy="14" r="7">
+
+</circle>
+                    </g>
+                </g>
+            </g>
+        </g>
+    </g>
+</svg>`;
+
+const svgIcon = L.divIcon({
+  html: svgString,
+  className: 'custom-svg-icon',
+  iconSize: [24, 24],
+  iconAnchor: [12, 24],
+  popupAnchor: [0, -36]
+});
+
 
 type Coordinates = [number, number][];
 const Markerwhatever: FC<any> = ({ coords, setFunc }) => {
@@ -12,7 +48,8 @@ const Markerwhatever: FC<any> = ({ coords, setFunc }) => {
   return (
     <div>
       <Marker
-        // icon={props.icon}
+      // @ts-ignore
+        icon={svgIcon}
         position={coords}
         eventHandlers={{
           click: (e) => {
