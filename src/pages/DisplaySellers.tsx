67deac6e378336538b8bd359192e8591c5037502
Hassan Shahid <hassanshahid234@gmail.com>
2024-10-18 11:42:12 +0500

    commit

--- src/pages/DisplaySellers.tsx
+++ src/pages/DisplaySellers.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header"
 import { useState } from "react";
 
+  
 
 const DisplaySellers = () => {
     const [search, setSearch] = useState("");
@@ -8,6 +9,8 @@ const DisplaySellers = () => {
     <div className="p-5 bg-slate-100 min-h-screen">
          {/* <!-- Header --> */}
       <Header search={search} setSearch={setSearch} heading="Sellers Listing"/>
+
+
     </div>
   )
 }
