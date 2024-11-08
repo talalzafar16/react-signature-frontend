52f8da035a52407943340662ca353bc9dd23f03e
Talal <talalzafar84@gmail.com>
2024-11-08 18:56:58 +0500

    .

--- src/config/apiEndpoint.ts
+++ src/config/apiEndpoint.ts
@@ -1,2 +1,2 @@
-export const API_ENDPOINT = 'https://signature-backend.vercel.app';
+export const API_ENDPOINT = "https://react-signature-backend.vercel.app";
 // export const API_ENDPOINT = 'http://localhost:3000';
