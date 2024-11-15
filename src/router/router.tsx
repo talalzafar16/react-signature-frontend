59095bedd00855a2a55e2197d56dd5ef5d466a00
Talal <talalzafar84@gmail.com>
2024-11-15 15:34:24 +0500

    .

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -6,22 +6,22 @@ import ExcelListing from "../pages/ExcelListing";
 import DisplayExcelData from "@/pages/DisplayExcelData";
 import ProtectedRoute from "./ProtectedRoute";
 import NotFound from "@/pages/NotFound";
-import LatLongFinderMap from "@/pages/LatlOngFinderMap";
+import LatLongFinderMap from "@/pages/LatLongFinderMap";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-          <Route path="/map" element={<Map />} />
-          <Route path="/LatLongFinderMap" element={<LatLongFinderMap />} />
+        <Route path="/map" element={<Map />} />
+        <Route path="/LatLongFinderMap" element={<LatLongFinderMap />} />
 
         <Route element={<ProtectedRoute />}>
           <Route path="/home/*" element={<Home />} />
           <Route path="/excelListing" element={<ExcelListing />} />
           <Route path="/displayExcelData/:id" element={<DisplayExcelData />} />
         </Route>
-        
+
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
