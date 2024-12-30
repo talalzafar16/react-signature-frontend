757f0a4bb707c826ecc87eb16f5f46224616315b
fizasaif123 <fizasaif02@gmail.com>
2024-12-30 15:28:12 +0500

    .'

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -5,6 +5,13 @@ import { useState, useLayoutEffect } from "react";
 import "../App.css";
 import "leaflet/dist/leaflet.css";
 // import MapUrl from "../assets/map/overlay10.png";
+
+import MapTile8 from "../assets/map/tiles/8.jpg";
+import MapTile7 from "../assets/map/tiles/7.jpg";
+import MapTile2 from "../assets/map/tiles/2.jpg";
+import MapTile3 from "../assets/map/tiles/3.jpg";
+import MapTile4 from "../assets/map/tiles/4.png";
+import MapTile5 from "../assets/map/tiles/5.png";
 import MapTile6 from "../assets/map/tiles/6.png";
 import MapTile9 from "../assets/map/9.png";
 import { API_ENDPOINT } from "../config/apiEndpoint";
@@ -31,7 +38,7 @@ const Map = () => {
   const [searchedPlot, setSearchedPlot] = useState<Models[] | []>([]);
   const [arratLATLONG, setArratLATLONG] = useState<Models[] | []>([]);
   const INITIAL_CENTER = [74.1936, 31.456];
-  const INITIAL_ZOOM = 15.5;
+  const INITIAL_ZOOM = 13.5;
   const [showSaleModal, setShowSaleModal] = useState<Boolean>(false);
   const [showEnquireyModal, setShowEnquireyModal] = useState<Boolean>(false);
   const [showFilterModal, setShowFilterModal] = useState<Boolean>(false);
@@ -96,7 +103,7 @@ const Map = () => {
     // @ts-ignore
     mapRef.current = new mapboxgl.Map({
       container: mapContainerRef.current,
-      // style: "mapbox://styles/mapbox/streets-v11",
+      style: "mapbox://styles/mapbox/streets-v11",
       // @ts-ignore
       center: INITIAL_CENTER,
       zoom: INITIAL_ZOOM,
@@ -165,12 +172,138 @@ const Map = () => {
       //   rotatePoint([bounds[1][0], bounds[1][1]], center, radians), // Top-right
       //   rotatePoint([bounds[0][0], bounds[1][1]], center, radians), // Top-left
       // ];
+      
       const Tile6Coordinates = [
-        [74.1957, 31.4732], // Bottom-left (Southwest) (decreased longitude)
-        [74.2108, 31.4732], // Bottom-right (Southeast)
-        [74.2102, 31.4541],  // Top-right (Northeast)
-        [74.1956, 31.4541],  // Top-left (Northwest) (decreased longitude)
+        [74.19562, 31.4628],  // Bottom-left (Southwest) (unchanged)
+        [74.20553, 31.4628],  // Bottom-right (Southeast) (reduced longitude)
+        [74.205507, 31.454136], // Top-right (Northeast) (reduced longitude)
+        [74.19563, 31.454132], // Top-left (Northwest) (unchanged)
       ];
+      
+      const Tile5Coordinates = [
+        [74.1833, 31.47291], // Bottom-left (Southwest) (longitude increased)
+        [74.1958, 31.47291], // Bottom-right (Southeast) (unchanged)
+        [74.19563, 31.45406], // Top-right (Northeast) (unchanged)
+        [74.1830, 31.45406], // Top-left (Northwest) (longitude increased)
+      ];
+      const Tile4Coordinates = [
+        [74.1708, 31.47291], // Bottom-left (Southwest)
+        [74.1833, 31.47291], // Bottom-right (Southeast) (aligned with Tile5's bottom-left)
+        [74.1830, 31.45406], // Top-right (Northeast) (aligned with Tile5's top-left)
+        [74.1705, 31.45406], // Top-left (Northwest)
+      ];
+      
+      const Tile3Coordinates = [
+        [74.19574, 31.46886], // Bottom-left (moved slightly left)
+        [74.19860, 31.46886], // Bottom-right (moved slightly left)
+        [74.19860, 31.462754], // Top-right (moved slightly left)
+        [74.19574, 31.462754], // Top-left (moved slightly left)
+      ];
+      
+      const Tile8Coordinates = [
+        [74.1830, 31.45399], // Bottom-left (Southwest) (longitude decreased)
+        [74.19563, 31.45399], // Bottom-right (Southeast) (longitude decreased)
+        [74.19546, 31.433298], // Top-right (Northeast) (longitude decreased)
+        [74.1829, 31.433298], // Top-left (Northwest) (longitude decreased)
+      ];
+      const Tile7Coordinates = [
+        [74.1703, 31.45398], // Bottom-left (Southwest) (longitude decreased)
+        [74.1830, 31.45398], // Bottom-right (Southeast) (matches Tile8's bottom-left longitude)
+        [74.1829, 31.43393], // Top-right (Northeast) (matches Tile8's top-left longitude)
+        [74.1702, 31.43393], // Top-left (Northwest) (longitude decreased)
+      ];
+      
+      
+      
+      
+      
+      
+      
+      const Tile2Coordinates = [
+        [74.18434, 31.48302], // Bottom-left (moved slightly right)
+        [74.201244, 31.48296], // Bottom-right (moved slightly right)
+        [74.200724, 31.469879], // Top-right (moved slightly right)
+        [74.18411, 31.469864], // Top-left (moved slightly right)
+      ];
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      const rotateCoordinates = (coords, angleDeg, center) => {
+        const angleRad = (angleDeg * Math.PI) / 180; // Convert to radians
+        const cosTheta = Math.cos(angleRad);
+        const sinTheta = Math.sin(angleRad);
+        
+        return coords.map(([x, y]) => {
+          const [cx, cy] = center; // Center of rotation
+          const dx = x - cx; // Translate point to origin
+          const dy = y - cy;
+          
+          // Apply rotation
+          const nx = dx * cosTheta - dy * sinTheta + cx;
+          const ny = dx * sinTheta + dy * cosTheta + cy;
+          
+          return [nx, ny];
+        });
+      };
+      
+      // Center of rotation (average of all coordinates)
+      const center = [
+        Tile2Coordinates.reduce((sum, [x]) => sum + x, 0) / Tile2Coordinates.length,
+        Tile2Coordinates.reduce((sum, [, y]) => sum + y, 0) / Tile2Coordinates.length,
+      ];
+      
+      // Apply rotation
+      const rotatedCoordinates = rotateCoordinates(Tile2Coordinates, -19.8, center);
+      
+      
+      
+      
+      
+
+// @ts-ignore
+mapRef.current.addSource("overlay-image-7", {
+  type: "image",
+  url: MapTile7,
+  coordinates: Tile7Coordinates,
+});
+// @ts-ignore
+mapRef.current.addSource("overlay-image-8", {
+  type: "image",
+  url: MapTile8,
+  coordinates: Tile8Coordinates,
+});
+// @ts-ignore
+mapRef.current.addSource("overlay-image-3", {
+  type: "image",
+  url: MapTile3,
+  coordinates: Tile3Coordinates,
+});
+      // @ts-ignore
+      mapRef.current.addSource("overlay-image-2", {
+        type: "image",
+        url: MapTile2,
+        coordinates: rotatedCoordinates,
+      });
+      
+      // @ts-ignore
+      mapRef.current.addSource("overlay-image-4", {
+        type: "image",
+        url: MapTile4,
+        coordinates: Tile4Coordinates,
+      });
+      // @ts-ignore
+      mapRef.current.addSource("overlay-image-5", {
+        type: "image",
+        url: MapTile5,
+        coordinates: Tile5Coordinates,
+      });
       // @ts-ignore
       mapRef.current.addSource("overlay-image-9", {
         type: "image",
@@ -184,8 +317,27 @@ const Map = () => {
         coordinates: Tile6Coordinates,
       });
 
+  
       // @ts-ignore
-      
+      mapRef.current.addLayer({
+        id: "overlay-layer-7",
+        type: "raster",
+        source: "overlay-image-7",
+      });
+      // @ts-ignore
+      mapRef.current.addLayer({
+        id: "overlay-layer-4",
+        type: "raster",
+        source: "overlay-image-4",
+      });
+
+      // @ts-ignore
+      mapRef.current.addLayer({
+        id: "overlay-layer-5",
+        type: "raster",
+        source: "overlay-image-5",
+      });
+      // @ts-ignore
       mapRef.current.addLayer({
         id: "overlay-layer-6",
         type: "raster",
@@ -193,10 +345,28 @@ const Map = () => {
       });
       // @ts-ignore
       mapRef.current.addLayer({
+        id: "overlay-layer-8",
+        type: "raster",
+        source: "overlay-image-8",
+      });
+      // @ts-ignore
+      mapRef.current.addLayer({
         id: "overlay-layer-9",
         type: "raster",
         source: "overlay-image-9",
       });
+         // @ts-ignore
+         mapRef.current.addLayer({
+          id: "overlay-layer-2",
+          type: "raster",
+          source: "overlay-image-2",
+        });
+         // @ts-ignore
+ mapRef.current.addLayer({
+  id: "overlay-layer-3",
+  type: "raster",
+  source: "overlay-image-3",
+});
       // @ts-ignore
       // mapRef.current.addLayer({
       //   id: "overlay-layer-6",
