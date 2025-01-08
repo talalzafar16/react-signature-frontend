7a6bee3e9621680c295f761908f8257acbcdb485
Talal <talalzafar84@gmail.com>
2025-01-08 15:46:08 +0500

    >

--- src/confidential/index.ts
+++ src/confidential/index.ts
@@ -1,4 +1,4 @@
-// const SERVER_URL = "https://signature-backend.vercel.app/";
-const SERVER_URL = "http://localhost:8082/";
+const SERVER_URL = "https://signature-backend.vercel.app/";
+// const SERVER_URL = "http://localhost:8082/";
 
 export { SERVER_URL };
